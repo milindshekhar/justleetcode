ee6ce73ba198fff04d706c5f3d7dc2c16c855c33
Milind Shekhar <43276934+milindshekhar@users.noreply.github.com>
2026-01-30 16:31:48 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.43%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int point=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[point]);
+                point++;
+            }
+        }
+        return;
+    }
+};
